5390b81780ea100f68387ad62dd96d0d47c28d41
Arif-dev00 <125997728+Arifbhrn@users.noreply.github.com>
2026-08-30 18:52:19 +0800

    add s3 bucket (#1)

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.62"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-bootcamp-terraform-nik-muhammad-arif-adlan"
+    key    = "terraform/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
