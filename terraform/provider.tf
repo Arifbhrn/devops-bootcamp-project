e39fcc1bf92bfb3d3ab1cf2a5fc0629d588ef334
Arif-dev00 <125997728+Arifbhrn@users.noreply.github.com>
2026-09-02 22:33:15 +0800

    ec2 (#4)
    * added ec2.tf
    
    * change map_public_ip_on_launch to false
    
    * add lockfile and aws_caller_identity
    
    * update on cidr_ipv4

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,9 +9,12 @@ terraform {
     bucket = "devops-bootcamp-terraform-nik-muhammad-arif-adlan"
     key    = "terraform/terraform.tfstate"
     region = "ap-southeast-1"
+    use_lockfile = true
   }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+data "aws_caller_identity" "current" {}
