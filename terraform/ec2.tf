9324d8c367edc02a90d675f3cf863a561de63717
Nik <arifbhrn0@gmail.com>
2026-09-02 22:29:43 +0800

    added ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,74 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+data "aws_iam_instance_profile" "my_ssm_profile" {
+  name = "EC2-SSM-ROLE"
+}
+
+resource "aws_eip" "eip-web-server" {
+  domain = "vpc"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = module.web-server.id
+  allocation_id = aws_eip.eip-web-server.id
+  depends_on = [ module.web-server ]
+}
+
+module "web-server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                   = "web-server"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.devops-vpc.public_subnets[0]
+  private_ip             = "10.0.0.5"
+  create_security_group  = false
+  vpc_security_group_ids = [module.devops-public-sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  # user_data = templatefile("userdata.sh", {})
+  tags = { Name = "web-server" }
+}
+
+module "ansible-controller" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                   = "ansible-controller"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.devops-vpc.private_subnets[0]
+  private_ip             = "10.0.0.135"
+  create_security_group  = false
+  vpc_security_group_ids = [module.devops-private-sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  # user_data = templatefile("userdata.sh", {})
+  tags = { Name = "ansible-controller" }
+}
+
+module "monitor-server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                   = "monitor-server"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.devops-vpc.private_subnets[0]
+  private_ip             = "10.0.0.136"
+  create_security_group  = false
+  vpc_security_group_ids = [module.devops-private-sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  # user_data = templatefile("userdata.sh", {})
+  tags = { Name = "monitor-server" }
+}
