0d671e5067322f292f0e04212c551ad6e589c6f3
Nik <arifbhrn0@gmail.com>
2026-09-10 20:47:33 +0800

    Remove this file. Inventroy will be dynamically generated on Ansible controller EC2 instance during boot

--- terraform/inventory.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "inventory" {
-  filename = "inventory.ini"
-  content = templatefile("inventory.ini.tftpl", {
-    web_server_ip = module.web-server.public_ip
-    monitoring_ip = module.monitor-server.private_ip
-    controller_ip = module.ansible-controller.private_ip
-  })
-}
