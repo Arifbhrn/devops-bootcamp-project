a25ab9fd604e8ce3752a4dee00d65ae268cb1b8c
Nik <arifbhrn0@gmail.com>
2026-09-13 13:33:23 +0800

    add iam.tf to access private ecr

---
+++ terraform/iam.tf
@@ -0,0 +1,28 @@
+data "aws_iam_role" "ssm_role" {
+  name = "EC2-SSM-ROLE"
+}
+
+resource "aws_iam_role_policy" "ecr_push_pull" {
+  name = "ecr-push-pull"
+  role = data.aws_iam_role.ssm_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
